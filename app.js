fd80245eda1d68663b80af2c6a2a170f7e7c85f7
GOPIKA MP <gopikaramesh360@gmail.com>
2024-06-13 13:49:27 +0530

    completed

--- app.js
+++ app.js
@@ -14,17 +14,37 @@ const generateHashedPassword = async (password) => {
     return bcrypt.hash(password, salt)
 }
 
-app.post("/signup",async (req,res)=>{
+app.post("/signup", async (req, res) => {
     let input = req.body
     let hashedPassword = await generateHashedPassword(input.password)
     console.log(hashedPassword)
     input.password = hashedPassword
     let blog = new blogmodel(input)
     blog.save()
-    res.json({"status":"success"})
+    res.json({ "status": "success" })
 })
 
+app.post("/signin", (req, res) => {
+    let input = req.body
+    blogmodel.find({ "emailid": req.body.emailid }).then(
+        (Response) => {
+            if (Response.length > 0) {
+                let dbPassword = Response[0].password
+                console.log(dbPassword)
+                bcrypt.compare(input.password, dbPassword, (error, isMatch) => {
+                    if (isMatch) {
+                        res.json({ "status": "success", "userId": Response[0]._id })
+                    } else {
+                        res.json({ "status": "incorrect password" })
+                    }
+                })
+            } else {
+                res.json({ "status": "User not found" })
+            }
+        }
+    ).catch()
+})
 
-app.listen(8000,()=>{
+app.listen(8000, () => {
     console.log("Server Started")
 })
